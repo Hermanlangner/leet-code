8416da9f01294b13256cca88430736483dd9eac5
Herman Langner <hermandlangner@gmail.com>
2024-08-25 21:10:58 +0200

    problem 3

--- problem_2/lib/problem2.ex
+++ problem_2/lib/problem2.ex
@@ -12,18 +12,39 @@ defmodule Problem2 do
       :world
 
   """
+
+  defmodule ListNode do
+    @type t :: %__MODULE__{
+            val: integer,
+            next: ListNode.t() | nil
+          }
+    defstruct val: 0, next: nil
+  end
+
   def add_two_numbers(l1, l2) do
-    a = Enum.reverse(l1) |> Enum.reduce(l1, 0, fn n, acc -> acc * 10 + n end)
-    b = Enum.reverse(l2) |> Enum.reduce(l2, 0, fn n, acc -> acc * 10 + n end)
+    a = find_number(l1, 1)
+    b = find_number(l2, 1)
     c = a + b
     reverse_number(c)
   end
 
-  def reverse_number(0), do: []
+  def find_number(nil, multiplier) do
+    0
+  end
+
+  def find_number(node, multiplier) do
+    node.val * multiplier + find_number(node.next, multiplier * 10)
+  end
+
+  def reverse_number(0), do: nil
 
   def reverse_number(n) do
     r = rem(n, 10)
     n = div(n, 10)
-    [r | reverse_number(n)]
+
+    %Node{
+      val: r,
+      next: reverse_number(n)
+    }
   end
 end
